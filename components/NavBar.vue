5abf04c86186c43be8969e7984ab7703d39906fa
Reza1290 <reza.muktasib@gmail.com>
2023-12-14 21:53:15 +0700

    add: setup font

---
+++ components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="flex justify-between items-center px-20 py-10">
+        <div>
+            <img src="" alt="logo">
+        </div>
+        <div class="flex items-center gap-5">
+            <div>Home</div>
+            <div>Explore</div>
+            <div>Play</div>
+        </div>
+    </nav>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules :['@nuxtjs/tailwindcss','@nuxtjs/google-fonts']
+  modules :['@nuxtjs/tailwindcss','@nuxtjs/google-fonts'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: ['~/assets/css/main.css'],
+  googleFonts: {
+    preload: true,
+    display: "swap",
+    families: {
+      // a simple name
+      
+      Poppins: {
+        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900]
+      },
+    }
+  }
 })
