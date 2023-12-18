f64c31baa73e0fd99040c420329fbce580a98cf3
iqbal-rahmatullah <iqbalrahmmatullah01@gmail.com>
2023-12-18 14:47:23 +0700

    Add data provinsi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules :['@nuxtjs/tailwindcss','@nuxtjs/google-fonts'],
+  devtools: { enabled: false },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   googleFonts: {
     preload: true,
     display: "swap",
     families: {
       // a simple name
-      
+
       Poppins: {
-        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900]
+        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
       },
-    }
-  }
+    },
+  },
 })
