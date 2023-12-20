b0881b936411b1ffb7a0aa74ab2c05ce60511050
akmalzidani <akmal20031003@gmail.com>
2023-12-20 20:26:27 +0700

    fix homepage + add hover animation, - footer and game section

---
+++ components/Home/About.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="h-[1024px] flex items-center justify-between center py-[147px] px-[101px]"
+  >
+    <div class="w-[58%] mr-3">
+      <h3 class="font-bold text-7xl text-primary">Apa itu LokaBestari?</h3>
+      <!-- <p class="mt-4 text-2xl italic font-normal text-accent">
+        <span class="font-medium">loka</span> /lo·ka/ = dunia; tempat
+      </p> -->
+      <p class="mt-4 text-2xl italic font-normal text-accent">
+        <span class="font-medium">bestari</span> /bes·ta·ri/ = luas dan dalam
+        pengetahuannya; berpendidikan baik; baik budi pekerti
+      </p>
+      <p class="text-2xl font-normal text-black mt-7">
+        LokaBestari merupakan situs web yang mempersembahkan keanekaragaman
+        budaya yang ada di Indonesia. <br />
+        <br />Di LokaBestari, kami membangun sebuah ruang digital yang
+        mempersembahkan kekayaan tradisi, seni, dan budaya dari penjuru
+        nusantara. Dari kesenian, hidangan otentik, dan masih banyak yang
+        lainnya. <br />
+        <br />LokaBestari memberi kemudahan bagi Anda untuk mengeksplor dan
+        mengedukasi Anda tentang warisan budaya yang ada di Indonesia.
+      </p>
+    </div>
+    <div class="box-border flex w-[30%] justify-end">
+      <div class="flex flex-wrap justify end">
+        <img
+          src="~/assets/img/home/about1.png"
+          class="max-w-[515px] max-h-[610px] rounded-3xl z-10 hover:scale-105 transition-all duration-500 hover:shadow-2xl"
+          alt=""
+        />
+        <img
+          src="~/assets/img/home/about2.png"
+          class="w-[473px] h-[329px] -mt-[55%] -ml-[15%] z-20 rounded-3xl outline outline-8 outline-[#EFFFF3] hover:outline-none hover:scale-105 transition-all duration-500 hover:shadow-2xl"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ components/Home/Hero.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+    class="container min-w-full min-h-[890px] -mt-[6%] flex justify-center items-center rounded-b-[120px] bg-[url('~/assets/img/home/hero.png')] bg-cover bg-center"
+  >
+    <div class="container flex flex-col items-center">
+      <p class="font-normal text-[32px] leading-none text-center text-white">
+        EKSPLOR KEKAYAAN BUDAYA INDONESIA
+      </p>
+      <div class="w-[180px] h-[4px] bg-[#8CFE5C] my-[9px]"></div>
+      <p
+        class="font-medium text-[57.715px] leading-none text-center text-white"
+      >
+        Selamat Datang di
+      </p>
+      <p
+        class="font-bold text-[105px] leading-none text-center bg-clip-text bg-gradient-to-b from-white via-white to-[#8cfe5c] text-transparent"
+      >
+        LokaBestari
+      </p>
+      <button
+        class="w-[203px] h-[59px] rounded-[13px] bg-[#8CFE5C] flex justify-center items-center mt-5"
+      >
+        <p class="text-2xl text-[#1F420F] font-medium">Jelajahi</p>
+        <div class="ml-[14px]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="27"
+            height="16"
+            viewBox="0 0 27 16"
+            fill="none"
+          >
+            <path
+              d="M18.8286 2L25 8.17142L18.8286 14.3428"
+              stroke="#1F420F"
+              stroke-width="2.17143"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M24.8 8.24316H2"
+              stroke="#1F420F"
+              stroke-width="2.17143"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
