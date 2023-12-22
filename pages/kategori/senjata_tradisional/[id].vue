8ffdd5232657c93de9a23014199659efd779d826
iqbal-rahmatullah <iqbalrahmmatullah01@gmail.com>
2023-12-22 22:56:45 +0700

    fix dikit

--- pages/kategori/senjata_tradisional/[id].vue
+++ pages/kategori/senjata_tradisional/[id].vue
@@ -1,1 +1,11 @@
-<template></template>
+<template>
+  <NavbarDark class="hidden lg:flex" />
+  <NavbarMobileDark class="lg:hidden"></NavbarMobileDark>
+  <DetailItem />
+  <div></div>
+</template>
+
+<script setup>
+import NavbarDark from "~/components/NavbarDark.vue"
+import DetailItem from "~/components/categories/senjata-tradisional/DetailItem.vue"
+</script>
